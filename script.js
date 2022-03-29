93d1ca45728c08e647f13cfa17f227bda02bda1c
Ana <rachel.anastasia.rose@gmail.com>
2022-03-30 01:22:08 +1100

    Hamburger menu changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".navbar__menu");
+const logo = document.querySelector(".navbar__logo");
 const menuLinks = document.querySelectorAll(".navbar__menu--item");
 
 hamburger.addEventListener("click", () => {
@@ -10,3 +11,6 @@ menuLinks.forEach((link) =>
         menu.classList.remove("navbar__menu--mobile");
     }),
 );
+logo.addEventListener("click", () => {
+    menu.classList.remove("navbar__menu--mobile");
+});
