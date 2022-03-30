5bcf5e75ddf2764b9e1cfcbba749b1da3acfe6cd
Ana <rachel.anastasia.rose@gmail.com>
2022-03-30 13:45:30 +1100

    mobile menu enhancement

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ hamburger.addEventListener("click", () => {
 menuLinks.forEach((link) =>
     link.addEventListener("click", () => {
         menu.classList.remove("navbar__menu--mobile");
+        iconToggle();
     }),
 );
 logo.addEventListener("click", () => {
     menu.classList.remove("navbar__menu--mobile");
+    if (menu.classList === open) {
+        iconToggle();
+    } else {
+        hamburgerIcon.style.display = "block";
+        crossIcon.style.display = "none";
+    }
 });
+
+const hamburgerIcon = document.getElementById("hamburger");
+const crossIcon = document.getElementById("cross");
+
+crossIcon.style.display = "none";
+
+const iconToggle = () => {
+    if (hamburgerIcon.style.display === "none") {
+        hamburgerIcon.style.display = "block";
+        crossIcon.style.display = "none";
+    } else {
+        hamburgerIcon.style.display = "none";
+        crossIcon.style.display = "block";
+    }
+};
